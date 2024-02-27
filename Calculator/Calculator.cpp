0c0cfc2520685032bb3ea195ffab4a204fd9be53
samuelmoores <samueldmoores@gmail.com>
2024-02-27 12:39:39 -0800

    first commit

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	//------- variables --------
+	int x = 0;
+	int y = 0;
+	int runCalculator = 1;
+
+	//while loop
+	//get input and do addition
+	while(runCalculator) {
+
+		std::cin >> x >> y;
+		std::cout << x + y << "\n";
+
+	}
+	
+
+}
+
