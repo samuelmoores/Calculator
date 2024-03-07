cda902a178fe9a286d9b94ce34ab2746065c086b
samuelmoores <samueldmoores@gmail.com>
2024-03-06 18:47:26 -0800

    more functions and main while loop added

--- Calculator_VisualStudio/Calculator/Calculator.cpp
+++ Calculator_VisualStudio/Calculator/Calculator.cpp
@@ -2,52 +2,83 @@
 
 enum States { ADD, SUBTRACT, MULTIPLY, DIVIDE };
 
+//adds two numbers together
+int Add(int a, int b)
+{
+	std::cout << "welcome to the add function\n";
+
+	return a + b;
+}
+
+//subtracts two numbers
+int Subtract(int x, int y)
+{
+	//output a message
+	std::cout << "welcome to the subtract function\n";
+
+	return x - y;
+}
+
+int Multiply(int x, int y)
+{
+	std::cout << "welcome to the multiply function\n";
+
+	return x * y;
+}
+
+int Divide(int a, int b) {
+	std::cout << "divide " << a << " and " << b << "\n";
+	if (b == 0) {
+		std::cout << "cant divide by 0\n";
+	}
+	else {
+		return a / b;
+	}
+}
+
+//what is the return value? int
 int main()
 {
 	//------- variables --------
 	int x = 0;
 	int y = 0;
-	int operation = 3;//could be 0, 1, 2, 3
+	int operation = 0;//could be 0, 1, 2, 3
+	bool runCalculator = true;
 
-	//get input
-	std::cin >> x >> y;
-
-	switch (operation)
+	//calculator loop
+	while (runCalculator)
 	{
-	case ADD:
-		std::cout << x + y;
-		break;
+		std::cout << "enter (0) add, (1) subtract, (2) multiply, (3) divide\n";
+		std::cin >> operation;
 
-	case SUBTRACT:
-		std::cout << x - y;
-		break;
+		//get input
+		std::cout << "enter the two operands:\n";
+		std::cin >> x >> y;
 
-	case MULTIPLY:
-		std::cout << x * y;
-		break;
-
-	case DIVIDE:
-		if (y == 0)
+		switch (operation)
 		{
+		case ADD:
+			//add
+			std::cout << Add(x, y) << "\n";
+			break;
 
-		}
-		else
-		{
-			std::cout << x / y;
+		case SUBTRACT:
+			//subtract
+			std::cout << Subtract(x, y) << "\n";
+			break;
 
+		case MULTIPLY:
+			//multipy
+			std::cout << Multiply(x, y) << "\n";
+			break;
+
+		case DIVIDE:
+			//divide
+			std::cout << Divide(x, y) << "\n";
+			break;
 		}
-		break;
 	}
-		
-
-
-
-
-
-
-
-
-	//calculate
 	
+	return 0;
 }
 
